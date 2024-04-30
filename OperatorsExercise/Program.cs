e8a86bd0558609142ed1be9054434eb739a46f0c
ryan wetmore <ryanwetmore1014@gmil.com>
2024-04-30 14:35:06 -0400

    Completed Operators exercise

---
+++ OperatorsExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace OperatorsExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a} / {b}  is {quotient} remainder {remainder}");
+
+            Console.WriteLine("Please enter the radius of your circle");
+
+            var userInput = Console.ReadLine(); 
+
+            var radius = double.Parse(userInput);
+
+            Console.WriteLine(AreaOfCircle(radius));
+
+
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(2, radius);
+
+            return area;
+            
+
+
+        }
+    }
+}
